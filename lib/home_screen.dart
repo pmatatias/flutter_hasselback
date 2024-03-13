067a22a84a50bbbfbbdb60eb30fc5017bbd3587a
Matatias <matatias97@gmail.com>
2024-03-13 13:51:49 +0700

    update directory

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hasselback/ovo/screen.dart';
+import 'package:flutter_hasselback/wawter/screen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -21,18 +22,33 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 4,
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            tileColor: Theme.of(context).colorScheme.inversePrimary,
-            title: const Text("OVO Loader Animation"),
-            onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const OvoView(),
-                )),
-            trailing: const Icon(Icons.chevron_right),
-          )
+        children: const <Widget>[
+          TileOn(screen: OvoView(), title: "OVO Loading Animation"),
+          TileOn(screen: WawterView(), title: "WaWter Animation"),
         ],
+      ),
+    );
+  }
+}
+
+class TileOn extends StatelessWidget {
+  const TileOn({super.key, required this.screen, required this.title});
+  final Widget screen;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 2.0),
+      child: ListTile(
+        tileColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => screen,
+            )),
+        trailing: const Icon(Icons.chevron_right),
       ),
     );
   }
---
+++ lib/wawter/screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WawterView extends StatefulWidget {
+  const WawterView({super.key});
+
+  @override
+  State<WawterView> createState() => _WawterViewState();
+}
+
+class _WawterViewState extends State<WawterView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
